dab39fda4f850b6e2375f43752c6ce50df6a71c7
Lesthy <103683378+Lesthy@users.noreply.github.com>
2022-08-31 17:39:20 +0300

    typo upd

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', () => {
     overlay.style.display = 'grid'; // changes display from none, to grid, on button click
-    overlay.classList.add('animate-overlay') //adds custom css class animate-overlay to overlay
+    overlay.classList.add('animate-overlay'); //adds custom css class animate-overlay to overlay
 })
